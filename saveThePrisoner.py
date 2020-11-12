d026b2f36806cf363976ec5a3f9ca805a5d0c4f1
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-11-12 12:45:25 -0600

    save the prisoner updated

--- saveThePrisoner.py
+++ saveThePrisoner.py
@@ -7,3 +7,14 @@ def saveThePrisoner(n,m, s):
     #m = number of treats
     #s = where the treats will start being passed out. 
     
+    #create an array to store the treats being passed out to the prisoner
+    # Use a  for loop 
+    m = m%n
+    s = s + m -1
+    if s > n:
+        s = s - n
+    
+
+    return s
+
+print(saveThePrisoner(352926151, 380324688, 94730870))
