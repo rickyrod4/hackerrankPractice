d8a649287dc6376b383a60f54198282e8ca4d355
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-11-17 14:17:48 -0600

    bublesort finished

--- bubblesort.py
+++ bubblesort.py
@@ -4,10 +4,13 @@
 
 def bubbleSort(arr):
     print('Original Array:', arr)
+    count = 0
     for i in range(len(arr)-1):
         for j in range(len(arr)-1-i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                count += 1
 
     print('New Array:', arr)
+    print('Count:', count)
 bubbleSort([10,1,7,2,3,2,8,6,4])
