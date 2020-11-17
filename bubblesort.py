678a8398d941213bbbb5fa7c0e0b5a95cfd9499a
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-11-17 11:05:30 -0600

    bubble sort complete

--- bubblesort.py
+++ bubblesort.py
@@ -5,8 +5,9 @@
 def bubbleSort(arr):
     print('Original Array:', arr)
     for i in range(len(arr)-1):
-        if arr[i] > arr[i+1]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     print('New Array:', arr)
-bubbleSort([8,7,2,6,4])
+bubbleSort([10,1,7,2,3,2,8,6,4])
