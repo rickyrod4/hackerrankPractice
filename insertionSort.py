7f083b6274f88c9022d856699d5d530e97ea5087
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-11-18 08:45:49 -0600

    Insertion Sort Complete

--- insertionSort.py
+++ insertionSort.py
@@ -3,9 +3,16 @@
 def insertionSort(arr):
     print('Original Array:', arr)
     for i in range(len(arr)-1):
-        if arr[i+1] < arr [i]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-        #for j in range(len(arr)-1-i):
+        for j in range(i+1,0,-1):
+            if arr[j] < arr [j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                print('Switch Happened!!!!')
+            else:
+                print('No Switch')
+            print("Iteration j:",j,"i:",i)
+            print('Array:', arr)
+            print('---------------------------')
+        print('*'*15)
     print('New Array:', arr)
 
 insertionSort([8,4,2,3,7])
